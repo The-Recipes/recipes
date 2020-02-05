4d8c78fc9657c9bf5fcca408a79f473fdcdbbb51
Daniel <dahnguyen@ucdavis.edu>
2020-02-05 11:45:52 -0800

    added express server with nodemon

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, './public')))
+
+app.get('/', (req,res) => res.send("Hello World!"))
+
+app.listen(PORT ,() => console.log(`Listening on port ${PORT}`))
